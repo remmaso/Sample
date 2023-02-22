5607fccc49e18fb571ec205d466881f7fdd5b807
Ralph Emmason <remmason@yahoo.com>
2023-02-22 09:14:02 -0400

    Adding of routes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import  connectDB  from './config/db.js'
-import products from './data/products.js'
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 
@@ -14,14 +14,7 @@ app.get('/', (req, res) =>{
     res.send('API is running...')
 })
 
-app.get('/api/products', (req, res) =>{
-    res.json(products)
-})
-
-app.get('/api/products/:id', (req, res) =>{
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product)
-})
+app.use('/api/products', productRoutes)
 
 process.on("unhandledRejection", err => {
     // if you want a stack always throw a error
